cf0d1a71a5fb96244eb6854b04a9a58376af1b19
Nikhil Bhaladhare <nikbhaladhare2104@gmail.com>
2024-04-10 21:25:58 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+function App() {
+
+  return (
+    <div className='App'>
+      <div className="card">
+        <img 
+          src='/images/qr-code.png'
+          className="card-img"
+        />
+        <div className="card-body">
+          <p className="card-text">
+            Improve your front-end  skills by building projects
+          </p>
+          <p className="card-description">
+            Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+          </p>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
